e8aaa073c7a08c715ebe8a7c36acd2a8a0833c7c
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-06-16 07:19:23 -0500

    Adventure Mode Off

--- js/script.js
+++ js/script.js
@@ -243,10 +243,15 @@ promiseProfile.then(profileDataHandler)
 
 
 
-// 2. ADVENTURE MODE - ANY GIT PROFILE
+/* 2. ADVENTURE MODE - ANY GIT PROFILE
 
 // Declare Key Element
 var inputNode = document.querySelector("#inputBox")
+
+
+var gitQuery = = function(userInput){
+
+}
 
 var userSearch = function(eventObj) {
     console.log(eventObj)
@@ -277,7 +282,7 @@ var userSearch = function(eventObj) {
 promiseUserProfile.then(profileDataHandler)
 promiseUserRepos.then(reposDataHandler)
 
-inputNode.addEventListener("keydown", userSearch)
+inputNode.addEventListener("keydown", userSearch) */
 
 
 
